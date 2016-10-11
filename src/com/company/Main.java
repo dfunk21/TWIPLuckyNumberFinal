0ca09ab27619b1e26172981645f8bbe805960229
dawsonfunk <dawsonfunk@gmail.com>
2016-10-10 21:06:45 -0400

    Updated TWIP Lucky Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,43 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Hello! Please enter your name below...");
-        String name = scan.nextLine();
-        randomGenerator randNum = new randomGenerator(name);
-        System.out.print("Nice to meet you " + name + "! Your lucky number is " + randNum.rd); //Random number
+
+        String nameList = "";
+        String randNumList = "";
+
+        while (true) {
+            System.out.println("Hello! Please enter your name below...");
+            String name = scan.nextLine();
+            nameList = nameList + name + " ";
+
+            randomGenerator randNum = new randomGenerator(name);
+            randNumList = randNumList + randNum.rd + " ";
+
+            System.out.println("Nice to meet you " + name + "! Your lucky number is " + randNum.rd); //Random number
+            System.out.println("Enter \"0\" to exit, \"1\" for a listing of names and lucky numbers, or \"2\" " +
+                    "to enter another name");
+            String secondResponse = scan.nextLine();
+
+            if (secondResponse.equalsIgnoreCase("0"))
+            {
+                break;
+            }
+            else if (secondResponse.equalsIgnoreCase("1"))
+            {
+                System.out.println("Names: " + nameList);
+                System.out.println("Lucky Numbers: " + randNumList);
+                break;
+            }
+            else if (secondResponse.equalsIgnoreCase("2"))
+            {
+
+            }
+            else
+            {
+                break;
+            }
+
+        }
 
 
     }
